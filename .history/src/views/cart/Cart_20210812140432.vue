d9103a35235fe9fc63a2b48b66e274a5cdda31ea
code-mouse <1185694992@qq.com>
2021-08-12 14:31:39 +0800

    测试1

---
+++ .history/src/views/cart/Cart_20210812140432.vue
@@ -0,0 +1,56 @@
+<!--
+ * @Author: 李佳辉
+ * @Date: 2021-08-12 13:21:03
+ * @LastEditors: 李佳辉
+ * @LastEditTime: 2021-08-12 14:04:32
+ * @Description: 测试
+-->
+<template>
+  <div class="cart">
+    <!--导航-->
+    <nav-bar class="nav-bar">
+      <div slot="center">购物车({{ cartLength }})</div>
+      <cart-list />
+    </nav-bar>
+    <!--商品列表-->
+    <cart-list />
+    <!--底部汇总-->
+    <cart-bottom-bar />
+  </div>
+</template>
+
+<script>
+import NavBar from "components/common/navbar/NavBar";
+import CartBottomBar from "./childComps/CartBottomBar";
+import CartList from "./childComps/CartList";
+
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Cart",
+  components: {
+    NavBar,
+    CartList,
+    CartBottomBar,
+  },
+  computed: {
+    //语法一
+    ...mapGetters(["cartLength"]),
+    //语法二
+    // ...mapGetters({
+    //   length: 'cartLength',
+    //   list: 'cartList',
+    // })
+  },
+};
+</script>
+
+<style scoped>
+.cart {
+  height: 100vh;
+}
+.nav-bar {
+  background-color: var(--color-tint);
+  color: #fff;
+}
+</style>
